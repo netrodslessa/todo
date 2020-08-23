adcb36eaa59a85b927be0e0cc1674a4085f963da
Rodrigo Lessa <netrods@gmail.com>
2020-08-23 10:34:20 -0300

    rota de filtro week

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,5 +1,5 @@
 const TaskModel = require('../model/TaskModel');
-const {startOfDay, endOfDay} = require('date-fns')
+const {startOfDay, endOfDay, startOfWeek, endOfWeek} = require('date-fns')
 
 const current = new Date();
 
@@ -117,6 +117,21 @@ class TaskController {
           .catch(error => {
             return res.status(500).json(error);
           });
+  }
+
+  async week(req, res){
+    await TaskModel
+          .find({
+            'macaddress': {'$in': req.body.macaddress},
+            'when': {'$gte': startOfWeek(current), '$lte': endOfWeek(current)}  
+          })
+          .sort('when')
+          .then(response => {
+            return res.status(200).json(response);
+          })
+          .catch(error => {
+            return res.status(500).json(error);
+          })
   }
 
 };
