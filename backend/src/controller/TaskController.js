c347f3a46faf29302d4a9828101195347aae9886
Rodrigo Lessa <netrods@gmail.com>
2020-08-23 10:27:02 -0300

    rota de filtro today

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,4 +1,5 @@
 const TaskModel = require('../model/TaskModel');
+const {startOfDay, endOfDay} = require('date-fns')
 
 const current = new Date();
 
@@ -101,6 +102,21 @@ class TaskController {
   .catch(error => {
     return res.status(500).json(error);
   })
+  }
+
+  async today(req, res){
+    await TaskModel
+          .find({ 
+            'macaddress': {'$in': req.body.macaddress},
+            'when': {'$gte': startOfDay(current), '$lte': endOfDay(current)} 
+          })
+          .sort('when')
+          .then(response =>{
+            return res.status(200).json(response);
+          })
+          .catch(error => {
+            return res.status(500).json(error);
+          });
   }
 
 };
--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -9,9 +9,10 @@ router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation, TaskController.update);
 router.get('/:id', TaskController.show);
 router.delete('/:id', TaskController.delete);
-router.put('/:id/:done', TaskController.done)
+router.put('/:id/:done', TaskController.done);
 
 router.get('/filter/all', MacaddressValidation, TaskController.all);
-router.get('/filter/late', MacaddressValidation, TaskController.late)
+router.get('/filter/late', MacaddressValidation, TaskController.late);
+router.get('/filter/today', MacaddressValidation, TaskController.today);
 
 module.exports = router;
