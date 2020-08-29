71cae3c0134abd7c446bb16f0d4146a3c66d9692
Rodrigo Lessa <netrods@gmail.com>
2020-08-29 10:25:18 -0300

    finalizado extrutura básica e iniciado home com componentes

---
+++ web/src/components/Footer/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 50px;
+  background: #20295f;
+  position: fixed;
+  bottom: 0;
+  display: flex; 
+  border-top: 5px #EE6B26 solid;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    color: #FFF;
+    display: flex;
+       
+  }
+`
